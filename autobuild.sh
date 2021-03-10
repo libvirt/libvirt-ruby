89c47c221520c63cf58248e2955ceedf989ca486
Andrea Bolognani <abologna@redhat.com>
2021-03-10 17:34:40 +0100

    maint: Drop Test-AutoBuild support
    The last Test-AutoBuild release was almost a decade ago; these
    days we use GitLab CI for pre-merge validation.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- autobuild.sh
+++ autobuild.sh
@@ -11,18 +11,10 @@ rake build
 rm -rf pkg
 rake package
 
-if [ -n "$AUTOBUILD_COUNTER" ]; then
-  EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
-else
-  NOW=`date +"%s"`
-  EXTRA_RELEASE=".$USER$NOW"
-fi
-
 if [ -f /usr/bin/rpmbuild ]; then
   ver=`grep '^PKG_VERSION' Rakefile | sed -e "s/PKG_VERSION=//" -e "s/'//g"`
   sed -e "s/\@VERSION\@/$ver/" < ruby-libvirt.spec > pkg/ruby-libvirt.spec
   rpmbuild --nodeps \
-     --define "extra_release $EXTRA_RELEASE" \
      --define "_sourcedir `pwd`/pkg" \
      -ba --clean pkg/ruby-libvirt.spec
 fi
