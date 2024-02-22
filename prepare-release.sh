77741d27296998bc3d6bf89b4818789e90afcbcb
Andrea Bolognani <abologna@redhat.com>
2024-02-22 11:35:33 +0100

    prepare-release: Call 'rake rpm'
    We already have all the logic that we need in the Rakefile,
    no need to duplicate it here.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -12,9 +12,5 @@ rm -rf pkg
 rake package
 
 if [ -f /usr/bin/rpmbuild ]; then
-  ver=`grep '^PKG_VERSION' Rakefile | sed -e "s/PKG_VERSION=//" -e "s/'//g"`
-  sed -e "s/\@VERSION\@/$ver/" < ruby-libvirt.spec > pkg/ruby-libvirt.spec
-  rpmbuild --nodeps \
-     --define "_sourcedir `pwd`/pkg" \
-     -ba --clean pkg/ruby-libvirt.spec
+    rake rpm
 fi
